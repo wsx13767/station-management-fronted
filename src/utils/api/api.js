4a86556323595e2225ec72dd3d4b9a50326b10df
Siang <wsx13767@gmail.com>
2022-04-19 22:18:14 +0800

    api及proxy設定

---
+++ src/utils/api/api.js
@@ -0,0 +1,21 @@
+const api =  ({url, method, headers, data }, option = {}) => {
+    console.log(url);
+    return fetch(url, {
+        method: method || 'GET',
+        body: data ? JSON.stringify(data) : null,
+        headers: headers || {
+            'content-type': 'application/json'
+        },
+        mode: 'cors',
+        ...option
+    }).then(res => {
+        if (!res.ok) return res.json().then((error) => alert(error.error));
+
+        return res.json();
+    })
+    .catch(e => {
+        alert("非預期錯誤");
+    })
+}
+
+export default api;
---
+++ src/utils/api/stationApi.js
@@ -0,0 +1,19 @@
+import api from './api.js'
+
+export default class station {
+    static getAllStation() {
+        return api({url: '/station'});
+    }
+
+    static getStation(id) {
+        return api({url: `/station/${id}`});
+    }
+
+    static insertStation(name) {
+        // const obj = {'name': name};
+        
+        // console.log(JSON.stringify(obj));
+        // return obj;
+        return api({url: '/station', method: 'POST', data: {name}});
+    }
+}
